2369260677546ad19526e2c1b52f7ea51442f844
zouhuigang <952750120@qq.com>
2019-06-13 12:35:26 +0800

    去掉文件名空格

--- zfile/file.go
+++ zfile/file.go
@@ -3,9 +3,11 @@ package zfile
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
+//判断文件是否存在
 func IsFileExist(filename string) bool {
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		return false
@@ -49,4 +51,38 @@ func PathExistsAndMkDir(_dir string) error {
 //获取文件后缀
 func Ext(path string) string {
 	return strings.ToLower(filepath.Ext(path))
+}
+
+/*
+处理路径
+windows下路径为:"D:\\workspacego\\src\\anooc-hujiao\\pdf\\赠险接口文档.docx" =>D:/workspacego/src/anooc-hujiao/pdf/赠险接口文档.docx
+linux下路径为:/root/mnt/
+其实建议使用 “/” 作为路径分隔符，windows 和 linux 系统都能正常使用。
+// 将 path 中平台相关的路径分隔符转换为 '/'
+ToSlash(path string) string
+// 将 path 中的 '/' 转换为系统相关的路径分隔符
+FromSlash(path string) string
+*/
+func FomartPath(path string) string {
+	sysType := runtime.GOOS
+
+	// if sysType == "linux" {
+	// 	// LINUX系统
+	// }
+
+	if sysType == "windows" {
+		// windows系统
+		return filepath.ToSlash(path)
+	}
+
+	return path
+}
+
+//检测文件名是否含有空格等特殊符号，如果有，则重命名
+func CheckFileName(fileName string) string {
+	// 去除空格
+	fileName = strings.Replace(fileName, " ", "", -1)
+	// 去除换行符
+	fileName = strings.Replace(fileName, "\n", "", -1)
+	return fileName
 }
--- zfileutil/zfile.go
+++ zfileutil/zfile.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/zouhuigang/package/zfile"
 )
 
 //返回的目录扫描结果
@@ -24,11 +26,22 @@ func ScanFiles(dir string) (fl []FileList, err error) {
 	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err == nil {
 			path = strings.Replace(path, "\\", "/", -1) //文件路径处理
+
+			//如果含有特殊字符等，则重命名
+			var new_file_name string = info.Name()
+			var check_file_name string = zfile.CheckFileName(info.Name())
+			var new_path string = path
+			if check_file_name != new_file_name {
+				new_file_name = check_file_name
+				new_path = filepath.Join(filepath.Dir(path), new_file_name) //+filepath.Ext(path)
+				os.Rename(path, new_path)
+			}
+
 			fl = append(fl, FileList{
 				IsDir:   info.IsDir(),
-				Path:    path,
+				Path:    new_path,
 				Ext:     strings.ToLower(filepath.Ext(path)),
-				Name:    info.Name(),
+				Name:    new_file_name,
 				Size:    info.Size(),
 				ModTime: info.ModTime().Unix(),
 			})
